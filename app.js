57ae8d8b9a40876081105e3414a4662d73865c87
Yogeswari NARAYASAMY <yogeswari.narayasamy@gmail.com>
2018-02-20 12:43:49 +0800

    follow colt's code

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect("mongodb://cicak:cicak@ds141028.mlab.com:41028/yelpcampcicak");
 //------------------------
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(__dirname + "../public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 //-----------------------------

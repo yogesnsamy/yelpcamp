77a49ca4058a2fb17b9362169e9c901fc859babd
Yogeswari NARAYASAMY <yogeswari.narayasamy@gmail.com>
2018-02-20 14:29:02 +0800

    copied colt's app.js file

--- app.js
+++ app.js
@@ -1,81 +1,55 @@
-var express 			= require("express"),
-	app 						= express(),
-	bodyParser 			= require("body-parser"),
-	mongoose 				= require("mongoose"),
-	methodOverride	=	require("method-override"),
-	flash						= require("connect-flash"),
-	passport				= require("passport"),
-	LocalStrategy		=	require("passport-local"),
-	Campground 			= require("./models/campground"),
-	Comment					= require("./models/comment"),
-	User						= require("./models/user"),
-	seedDB					= require("./seeds"); //to initialize the db
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+ 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
 
-var commentRoutes 		= require("./routes/comments"),
-		campgroundRoutes 	= require("./routes/campgrounds"),
-		indexRoutes 			=	require("./routes/index");
-
-//mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient:true});
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://cicak:cicak@ds141028.mlab.com:41028/yelpcampcicak");
-
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static(__dirname+"/public"));
-// app.use(methodOverride("_method"));
-// app.set("view engine","ejs");
-// app.use(flash());
-
-//------------------------
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "../public"));
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-//-----------------------------
+// seedDB(); //seed the database
 
-
-
-
-// initialize data
-// seedDB();
-
-//config passport
-app.use(require("express-session")(
-	{
-		secret: "mera naam cicak",
-		resave: false,
-		saveUninitialized: false
-	}
-));
-
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//send this parm to routes to see if a user is logged in. if not signed in, user = undefined
-app.use(function(req,res,next){
-	res.locals.currentUser = req.user;
-	res.locals.success = req.flash("success");
-	res.locals.error = req.flash("error");
-	next();
-})
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
 
-app.use("/",indexRoutes);
-app.use("/campgrounds",campgroundRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-//	app.listen(3000,function(){
-	console.log("connected");
-})
-
-//middleware function
-function isLoggedIn(req,res,next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/login");
-}
+   console.log("The YelpCamp Server Has Started!");
+});

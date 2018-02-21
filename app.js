befe339dcc8e5c06ff2841f0f6b326c2063b306c
Yogeswari NARAYASAMY <yogeswari.narayasamy@gmail.com>
2018-02-21 16:44:09 +0800

    revert

--- app.js
+++ app.js
@@ -1,74 +1,81 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
-    
-//requiring routes
-var commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
- 
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
-mongoose.connect(url);
+var express 			= require("express"),
+	app 						= express(),
+	bodyParser 			= require("body-parser"),
+	mongoose 				= require("mongoose"),
+	methodOverride	=	require("method-override"),
+	flash						= require("connect-flash"),
+	passport				= require("passport"),
+	LocalStrategy		=	require("passport-local"),
+	Campground 			= require("./models/campground"),
+	Comment					= require("./models/comment"),
+	User						= require("./models/user"),
+	seedDB					= require("./seeds"); //to initialize the db
 
+var commentRoutes 		= require("./routes/comments"),
+		campgroundRoutes 	= require("./routes/campgrounds"),
+		indexRoutes 			=	require("./routes/index");
+
+//mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient:true});
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://cicak:cicak@ds141028.mlab.com:41028/yelpcampcicak");
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static(__dirname+"/public"));
+// app.use(methodOverride("_method"));
+// app.set("view engine","ejs");
+// app.use(flash());
+
+//------------------------
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "../public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// seedDB(); //seed the database
+//-----------------------------
 
-// PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
-    resave: false,
-    saveUninitialized: false
-}));
+
+
+
+// initialize data
+// seedDB();
+
+//config passport
+app.use(require("express-session")(
+	{
+		secret: "mera naam cicak",
+		resave: false,
+		saveUninitialized: false
+	}
+));
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
-});
+//send this parm to routes to see if a user is logged in. if not signed in, user = undefined
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+})
 
-app.use("/", indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/",indexRoutes);
+app.use("/campgrounds",campgroundRoutes);
+app.use("/campgrounds/:id/comments",commentRoutes);
 
-//var port = process.env.PORT || 3000;
-var ipaddress = process.env.IP || "127.0.0.1";
 
-// --------------
-const port = 3000;
-const PORT = process.env.PORT || port;
-const HOST = process.env.BASE_URL || 'localhost';
-//const baseUrl = `http://${HOST}:${PORT}`;
-
-console.log("process.env.PORT: " + process.env.PORT);
-
-//server.set('port', process.env.PORT);
-
-//---------------
-console.log("url: " + url);
-console.log("PORT: "+PORT);
-console.log("ipaddress: "+ipaddress);
 app.listen(process.env.PORT, process.env.IP, function(){
-//    app.listen(port, function(){
-    console.log("process.env.IP 2: "+process.env.IP)
-   console.log("The YelpCamp Server Has Started!");
-});
+//	app.listen(3000,function(){
+	console.log("connected");
+})
 
+//middleware function
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}

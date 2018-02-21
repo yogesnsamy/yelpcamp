b92f6d5299f5bd3ef87cd84b9cc938ff9eff237b
Yogeswari NARAYASAMY <yogeswari.narayasamy@gmail.com>
2018-02-21 17:32:59 +0800

    remove IP from app.listen

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+//app.listen(process.env.PORT, process.env.IP, function(){
+    app.listen(process.env.PORT, function(){
 //	app.listen(3000,function(){
     console.log("connected");
     console.log(process.env.IP);
